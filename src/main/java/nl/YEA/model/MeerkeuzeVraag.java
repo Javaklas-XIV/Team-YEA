d25228225d7562f2e3d10df8eccb1071c03ded35
ElsVH-b <edj.vermeulen-de.haan@belastingdienst.nl>
2024-05-10 08:47:35 +0200

    vragenlijst met eenvoudige vragen samengesteld

--- src/main/java/nl/YEA/model/MeerkeuzeVraag.java
+++ src/main/java/nl/YEA/model/MeerkeuzeVraag.java
@@ -7,10 +7,16 @@ public class MeerkeuzeVraag extends Vraag {
     private int minAantalKeuzes;
     private List<String> keuzemogelijkheden;
 
-    public MeerkeuzeVraag(String beschrijving, int maxAantalKeuzes, int minAantalKeuzes, List<String> keuzemogelijkheden) {
+    public MeerkeuzeVraag(String beschrijving, List<String> keuzemogelijkheden) {
         super(beschrijving);
+        this.maxAantalKeuzes = 1;
+        this.minAantalKeuzes = 1;
+        this.keuzemogelijkheden = keuzemogelijkheden;
+    }
+
+    public MeerkeuzeVraag(String beschrijving, int maxAantalKeuzes, int minAantalKeuzes, List<String> keuzemogelijkheden) {
+        this(beschrijving, keuzemogelijkheden);
         this.maxAantalKeuzes = maxAantalKeuzes;
         this.minAantalKeuzes = minAantalKeuzes;
-        this.keuzemogelijkheden = keuzemogelijkheden;
     }
 }
--- src/main/java/nl/YEA/model/OpenVraag.java
+++ src/main/java/nl/YEA/model/OpenVraag.java
@@ -3,6 +3,10 @@ package nl.YEA.model;
 public class OpenVraag extends Vraag {
     private int maxAantalLetters;
 
+    public OpenVraag(String beschrijving) {
+        super(beschrijving);
+    }
+
     public OpenVraag(String beschrijving, int maxAantalLetters) {
         super(beschrijving);
         this.maxAantalLetters = maxAantalLetters;
