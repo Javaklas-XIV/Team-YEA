047593b8400e8f07d9f7f025ba512d9c496e83fe
ElsVH-b <edj.vermeulen-de.haan@belastingdienst.nl>
2024-05-14 11:03:05 +0200

    numeriekAntwoordController geïmplementeerd

--- src/main/java/nl/YEA/controller/NumeriekAntwoordController.java
+++ src/main/java/nl/YEA/controller/NumeriekAntwoordController.java
@@ -1,15 +1,17 @@
 package nl.YEA.controller;
 
 import nl.YEA.Singleton;
+import nl.YEA.exceptions.OnjuistAntwoordException;
 import nl.YEA.model.NumeriekAntwoord;
-import nl.YEA.model.NumeriekeVraag;
 
 public class NumeriekAntwoordController extends AntwoordController {
     private NumeriekeVraagController nvController;
 
-    public void setAntwoord(int antwoord) {
-        if (antwoordGoedgekeurd(antwoord)) {
-        }
+    public void addAntwoord(int vraagNr, int antwoord) {
+        if (antwoordGoedgekeurd(antwoord))
+            Singleton.getInstance().getRepo().addAntwoord(new NumeriekAntwoord(vraagNr, antwoord));
+        else
+            throw new OnjuistAntwoordException();
     }
 
     private boolean antwoordGoedgekeurd(int antwoord) {
